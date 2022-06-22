f52854dc2ede3956d7f25eab9eb7e75a9fc46652
Adrian Posada <adrian-924@hotmail.com>
2022-06-21 19:46:23 -0500

    Se agrega el objeto clients_table

--- pv.py
+++ pv.py
@@ -1,10 +1,11 @@
 import click 
 from clients import commands as clients_commands
 
-
+CLIENTS_TABLE = ".clients.csv"         
 @click.group
 @click.pass_context
 def cli(ctx):
     ctx.obj = {}
+    ctx.obj["clients_table"] = CLIENTS_TABLE
 
 cli.add_command(clients_commands.all)     #-----> Del modulo clients se importa commands como clients_commands y dentro del modulo se usa la variable all q es un alias de clients
